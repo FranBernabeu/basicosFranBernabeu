23c6f3f3bfdf705d0b022b139bcb4d833e45015d
FranBernabeu <fran.bernabeu88@gmail.com>
2018-10-24 23:23:36 +0200

    Commit final cadenas

--- BasicosFranBernabeu.java
+++ BasicosFranBernabeu.java
@@ -29,6 +29,11 @@ public class BasicosFranBernabeu {
         System.out.println(cadenas3);
         System.out.println("Nombre completo");
         System.out.println(cadenas4);
+        System.out.println("caracter 20");
+        System.out.println(cadenas4.charAt(20));
+        System.out.println("Longuitud Nombre");
+        System.out.println(cadenas4.length());
+       
        }
 
        }
