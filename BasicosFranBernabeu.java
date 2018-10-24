b49e8858eb2e7fe6b1c3d88d34e12292446616cf
FranBernabeu <fran.bernabeu88@gmail.com>
2018-10-24 22:19:44 +0200

    Commit exito numerico completa

--- BasicosFranBernabeu.java
+++ BasicosFranBernabeu.java
@@ -26,12 +26,24 @@ public class BasicosFranBernabeu {
         //Ejemplos de asignación de valores a las variables de tipos básicos
         entero = 10;
         doble = 5.6;
-
+        elMasPeque = 1;
+        chiquitin = 125;
+        duplicaEntero = 523698710;
+        grande = 500;
+        
         //Mostrar en pantalla el contendido de las variables de tipos básicos.
         System.err.println("Valor entero");
         System.out.println(entero);
         System.out.println("Valor doble");
         System.out.println(doble);
+        System.out.println("Menor unidad");
+        System.out.println(elMasPeque);
+        System.out.println("Valor de cien");
+        System.out.println(chiquitin);
+        System.out.println("Valor muy grande");
+        System.out.println(duplicaEntero);
+        System.out.println("Valor redondo");
+        System.out.println(grande);
     }
          public static void main(String[] args) {
         numericos();
